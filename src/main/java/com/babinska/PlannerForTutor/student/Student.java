ebcf1a55170f98ad404105546f318f3655d37081
Joanna Babinska <babinskaasia52@gmail.com>
2023-05-29 11:36:16 +0200

    modify Student.java

--- src/main/java/com/babinska/PlannerForTutor/student/Student.java
+++ src/main/java/com/babinska/PlannerForTutor/student/Student.java
@@ -1,12 +1,17 @@
 package com.babinska.PlannerForTutor.student;
 
+import com.babinska.PlannerForTutor.lesson.Lesson;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
+
 @Entity
+@Data
 public class Student {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +21,7 @@ public class Student {
   private String email;
   private String phoneNumber;
   private LocalDate dateOfBirth;
-  private Set <Lesson> lessonsId;
+  private Set<Lesson> lessonsId;
   private SchoolClass schoolClass;
   private String school;
 }
