260d415ca65ae1ca0dc5ce74637147c790425bac
Joanna Babinska <babinskaasia52@gmail.com>
2023-05-30 10:21:10 +0200

    add student.sql

---
+++ src/main/resources/db/testdata/student.sql
@@ -0,0 +1,4 @@
+INSERT INTO student ( first_name, last_name, email, phone_number, date_of_birth, school_class)
+VALUES
+('Joanna','Nowik', 'joanna@nowik.pl','698852236','2000-12-01','HIGH_SCHOOL_1TH_GRADE');
+
