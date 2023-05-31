74ca63d9f75a8077ccadba3da7f93214149b7ad9
Joanna Babinska <babinskaasia52@gmail.com>
2023-05-31 13:18:08 +0200

    add PhoneNumberValidator.java and PhoneNumber.java

---
+++ src/main/java/com/babinska/PlannerForTutor/student/constraint/PhoneNumberValidator.java
@@ -0,0 +1,12 @@
+package com.babinska.PlannerForTutor.student.constraint;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
+  @Override
+  public boolean isValid(String contactField, ConstraintValidatorContext cxt) {
+    return contactField == null || contactField.matches("[0-9]+")
+            && (contactField.length() > 8) && (contactField.length() < 12);
+  }
+}
