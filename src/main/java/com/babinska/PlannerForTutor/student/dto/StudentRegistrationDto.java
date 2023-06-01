ef6c420dfdfbeb65641733f29b24a2cae786fdd4
Joanna Babinska <babinskaasia52@gmail.com>
2023-06-01 06:45:14 +0200

    add validation annotations in StudentRegistrationDto and StudentUpdateDto

--- src/main/java/com/babinska/PlannerForTutor/student/dto/StudentRegistrationDto.java
+++ src/main/java/com/babinska/PlannerForTutor/student/dto/StudentRegistrationDto.java
@@ -1,6 +1,10 @@
 package com.babinska.PlannerForTutor.student.dto;
 
 import com.babinska.PlannerForTutor.student.SchoolClass;
+import com.babinska.PlannerForTutor.student.constraint.PhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +13,17 @@ import java.time.LocalDate;
 @Builder
 @Data
 public class StudentRegistrationDto {
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @Email
+    @NotNull
     private String email;
+    @NotNull
+    @PhoneNumber
     private String phoneNumber;
+    @Past
     private LocalDate dateOfBirth;
     private SchoolClass schoolClass;
   }
--- src/main/java/com/babinska/PlannerForTutor/student/dto/StudentUpdateDto.java
+++ src/main/java/com/babinska/PlannerForTutor/student/dto/StudentUpdateDto.java
@@ -1,6 +1,10 @@
 package com.babinska.PlannerForTutor.student.dto;
 
 import com.babinska.PlannerForTutor.student.SchoolClass;
+import com.babinska.PlannerForTutor.student.constraint.PhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +13,17 @@ import java.time.LocalDate;
 @Data
 public class StudentUpdateDto {
     private Long id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @Email
+    @NotNull
     private String email;
+    @PhoneNumber
+    @NotNull
     private String phoneNumber;
+    @Past
     private LocalDate dateOfBirth;
     private SchoolClass schoolClass;
   }
