98100edfe1a3ef1049fa239e347d9c92913e52b4
Joanna Babinska <babinskaasia52@gmail.com>
2023-06-01 08:07:27 +0200

    add shouldReplaceStudent test

--- src/test/java/com/babinska/PlannerForTutor/student/StudentServiceTest.java
+++ src/test/java/com/babinska/PlannerForTutor/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.babinska.PlannerForTutor.student;
 
+import com.babinska.PlannerForTutor.student.dto.StudentDto;
 import com.babinska.PlannerForTutor.student.dto.StudentDtoMapper;
 import com.babinska.PlannerForTutor.student.dto.StudentRegistrationDto;
 import org.junit.jupiter.api.Test;
@@ -14,10 +15,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentServiceTest {
 
   @Autowired
-  private StudentService underTest;
+  private StudentService studentService;
 
   @Autowired
   private StudentRepository studentRepository;
+
 
   @Test
   public void shouldSaveNewStudent() {
@@ -30,24 +32,56 @@ class StudentServiceTest {
             .phoneNumber("589996325")
             .schoolClass(SchoolClass.ELEMENTARY_SCHOOL_7TH_GRADE)
             .build();
-    Student studentToSave = StudentDtoMapper.map(studentRegistrationDto);
 
     //when
-    studentRepository.save(studentToSave);
+    StudentDto savedStudent = studentService.saveStudent(studentRegistrationDto);
 
     //then
-    Student savedStudent = studentRepository.findAll().stream()
-            .filter(student -> student.getFirstName().equals("testFirstName"))
-            .findFirst()
-            .get();
-
+    Student studentInDatabase = studentRepository.findById(savedStudent.getId()).get();
     assertAll(
-            () -> assertEquals("testFirstName", savedStudent.getFirstName()),
-            () -> assertEquals("testLastName",  savedStudent.getLastName()),
-            () -> assertEquals(LocalDate.of(2008,12,1), savedStudent.getDateOfBirth()),
-            () -> assertEquals("test@email.com", savedStudent.getEmail()),
-            () -> assertEquals("589996325", savedStudent.getPhoneNumber()),
-            () -> assertEquals(SchoolClass.ELEMENTARY_SCHOOL_7TH_GRADE, savedStudent.getSchoolClass())
+            () -> assertEquals("testFirstName", studentInDatabase.getFirstName()),
+            () -> assertEquals("testLastName",  studentInDatabase.getLastName()),
+            () -> assertEquals(LocalDate.of(2008,12,1), studentInDatabase.getDateOfBirth()),
+            () -> assertEquals("test@email.com", studentInDatabase.getEmail()),
+            () -> assertEquals("589996325", studentInDatabase.getPhoneNumber()),
+            () -> assertEquals(SchoolClass.ELEMENTARY_SCHOOL_7TH_GRADE, studentInDatabase.getSchoolClass())
     );
+  }
+  @Test
+  public void shouldReplaceStudent(){
+    //given
+    StudentRegistrationDto studentToSave= StudentRegistrationDto.builder()
+            .firstName("testFirstName")
+            .lastName("testLastName")
+            .dateOfBirth(LocalDate.of(2008, 12, 1))
+            .email("test@email.com")
+            .phoneNumber("589996325")
+            .schoolClass(SchoolClass.ELEMENTARY_SCHOOL_7TH_GRADE)
+            .build();
+
+    StudentRegistrationDto studentToReplace = StudentRegistrationDto.builder()
+            .firstName("testReplaceFirstName")
+            .lastName("testReplaceLastName")
+            .dateOfBirth(LocalDate.of(2008, 12, 1))
+            .email("testReplace@email.com")
+            .phoneNumber("123456789")
+            .schoolClass(SchoolClass.ELEMENTARY_SCHOOL_8TH_GRADE)
+            .build();
+
+    //when
+    StudentDto savedStudent = studentService.saveStudent(studentToSave);
+    StudentDto replacingStudent = studentService.replaceStudent(studentToReplace, savedStudent.getId());
+
+    //then
+    assertAll(
+            () -> assertEquals("testReplaceFirstName", replacingStudent.getFirstName()),
+            () -> assertEquals("testReplaceLastName", replacingStudent.getLastName()),
+            () -> assertEquals(LocalDate.of(2008, 12, 1), replacingStudent.getDateOfBirth()),
+            () -> assertEquals("testReplace@email.com", replacingStudent.getEmail()),
+            () -> assertEquals("123456789", replacingStudent.getPhoneNumber()),
+            () -> assertEquals(SchoolClass.ELEMENTARY_SCHOOL_8TH_GRADE, replacingStudent.getSchoolClass())
+    );
+
+
   }
 }
