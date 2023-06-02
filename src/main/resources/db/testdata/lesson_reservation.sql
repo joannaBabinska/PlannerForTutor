1ba425c76e51ea1574e5276a2ac568b812f2a84e
Joanna Babinska <babinskaasia52@gmail.com>
2023-06-02 08:23:18 +0200

    add testdata

---
+++ src/main/resources/db/testdata/lesson_reservation.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+lesson_reservation(lesson_id,topic,start_time,end_time,reservation_date,duration_in_minutes)
+VALUES
+(5,'geometria anlityczna - funkcja liniowa','13:00:00','14:30:00','2023-01-05',90),
+(5,'geometria - trójkąty','14:45:00','15:30:00','2023-01-05',45),
+(5,'geometria anlityczna - funkcja liniowa','15:30:00','16:30:00','2023-01-05',90),
+(2,'wyrażenia algebraiczne','16:50:00','18:20:00','2023-01-05',90),
+(2,'wyrażenia algebraiczne','18:30:00','19:50:00','2023-01-05',90),
+(3,'rozwiązywanie układów równań','20:00:00','21:00:00','2023-01-05',90);
---
+++ src/main/resources/db/testdata/lesson_reservation_students.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+lesson_reservation_students(lesson_reservation_id,students_id)
+VALUES
+(1,1),
+(2,2),
+(3,3),
+(3,4),
+(3,4),
+(4,5),
+(5,4),
+(5,4),
+(5,5),
+(6,6);
