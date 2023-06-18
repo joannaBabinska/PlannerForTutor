1f843ed7c188ae9e2d275f7f55995b7fc90396c0
Joanna Babinska <babinskaasia52@gmail.com>
2023-06-18 14:43:30 +0200

    add vacation functionality

---
+++ src/main/java/com/babinska/PlannerForTutor/vacation/VacationController.java
@@ -0,0 +1,22 @@
+package com.babinska.PlannerForTutor.vacation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/vacation")
+public class VacationController {
+
+  private final VacationService vacationService;
+
+  @GetMapping
+  ResponseEntity<List<Vacation>> getVacationDate(){
+  return ResponseEntity.ok(vacationService.getVacationDates());
+  }
+}
---
+++ src/main/resources/db/testdata/vacation.sql
@@ -0,0 +1,5 @@
+INSERT INTO vacation(vacation_day)
+VALUES
+('2023-06-05'),
+('2023-03-25'),
+('2023-07-12');
