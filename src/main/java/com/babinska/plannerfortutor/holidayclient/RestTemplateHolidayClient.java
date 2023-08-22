5d2e09d0a1da6c1e63d5ff59c1c1e23dfe788776
Mateusz Krajewski <maatsior@gmail.com>
2023-08-22 19:49:53 +0200

    refactor HolidayClient

--- src/main/java/com/babinska/plannerfortutor/holidayclient/RestTemplateHolidayClient.java
+++ src/main/java/com/babinska/plannerfortutor/holidayclient/RestTemplateHolidayClient.java
@@ -3,22 +3,18 @@ package com.babinska.plannerfortutor.holidayclient;
 import com.babinska.plannerfortutor.configuration.PlannerProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Set;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "holidayClient", name = "type", havingValue = "rest-template")
-public class RestTemplateHolidayClient implements HolidayClient {
+class RestTemplateHolidayClient implements HolidayClient {
 
   private final RestTemplate restTemplate;
   private final PlannerProperties plannerProperties;
-
-  public HolidayClient holidayClient() {
-    return new RestTemplateHolidayClient(restTemplate, plannerProperties);
-  }
 
   @Override
   public Set<Holiday> getHolidays() {
