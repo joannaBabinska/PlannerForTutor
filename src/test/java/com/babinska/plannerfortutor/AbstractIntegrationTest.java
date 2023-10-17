9da3b2c5b99341fd1a204a74ea84f22c5e2efbcf
Mateusz Krajewski <maatsior@gmail.com>
2023-10-17 14:04:26 +0200

    Fix tests

---
+++ src/test/java/com/babinska/plannerfortutor/AbstractIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.babinska.plannerfortutor;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+}
--- src/test/java/com/babinska/plannerfortutor/PlannerForTutorApplicationTests.java
+++ src/test/java/com/babinska/plannerfortutor/PlannerForTutorApplicationTests.java
@@ -1,10 +1,8 @@
 package com.babinska.plannerfortutor;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlannerForTutorApplicationTests {
+class PlannerForTutorApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
