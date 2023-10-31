03975296a32a8e9a3c1902bd4f227df84b298bac
Mateusz Krajewski <maatsior@gmail.com>
2023-10-31 19:43:32 +0100

    add PreConfiguredUsersService

--- src/main/java/com/babinska/plannerfortutor/configuration/SecurityConfiguration.java
+++ src/main/java/com/babinska/plannerfortutor/configuration/SecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.babinska.plannerfortutor.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,8 +27,10 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .csrf(CsrfConfigurer::disable)
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(antMatcher("/auth/**")).permitAll()
+                    .requestMatchers(PathRequest.toH2Console()).permitAll()
                     .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
--- src/main/java/com/babinska/plannerfortutor/user/UserService.java
+++ src/main/java/com/babinska/plannerfortutor/user/UserService.java
@@ -1,5 +1,6 @@
 package com.babinska.plannerfortutor.user;
 
+import com.babinska.plannerfortutor.configuration.PreConfiguredUsersConfiguration;
 import com.babinska.plannerfortutor.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,6 +36,22 @@ public class UserService {
     return savedUser;
   }
 
+  public void create(PreConfiguredUsersConfiguration.User user) {
+
+    // TODO validate unique email; strong password (8 chars, big letter, numeric sing, special sign, etc...)
+
+    User userToSave = User.builder()
+        .firstName(user.getFirstName())
+        .lastName(user.getLastName())
+        .email(user.getEmail())
+        .password(passwordEncoder.encode(user.getPassword()))
+        .role(user.getRole())
+        .build();
+
+    User savedUser = userRepository.save(userToSave);
+
+    log.info("Saved new User with id: {} and email: {}", savedUser.getId(), savedUser.getEmail());
+  }
 
   public UserDetails findByEmail(final String email) {
     return userRepository.findByEmail(email)
