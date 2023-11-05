1829098349dc5be8d3e2de840d077b6cea6db50c
Joanna Babińska <joanna.babinska@programisci.eu>
2023-11-05 21:05:14 +0100

    add demo permissions

--- src/main/java/com/babinska/plannerfortutor/configuration/SecurityConfiguration.java
+++ src/main/java/com/babinska/plannerfortutor/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -13,11 +14,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.babinska.plannerfortutor.user.Permission.ADMIN_READ;
+import static com.babinska.plannerfortutor.user.Role.ADMIN;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
   private final JwtAuthenticationFilter jwtAuthFilter;
@@ -30,6 +35,8 @@ public class SecurityConfiguration {
             .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(antMatcher("/auth/**")).permitAll()
+                    .requestMatchers(antMatcher("/statistic/**")).hasAnyRole(ADMIN.name())
+                    .requestMatchers(GET,"/statistic/**").hasAnyAuthority(ADMIN_READ.name())
                     .requestMatchers(PathRequest.toH2Console()).permitAll()
                     .anyRequest().authenticated()
             )
--- src/main/java/com/babinska/plannerfortutor/user/User.java
+++ src/main/java/com/babinska/plannerfortutor/user/User.java
@@ -3,11 +3,9 @@ package com.babinska.plannerfortutor.user;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @Setter
 @Getter
@@ -30,7 +28,7 @@ public class User implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority(role.name()));
+    return role.getAuthorities();
   }
 
   @Override
